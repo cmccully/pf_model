a8616727fd274f9cfce66ac9e330d0c742f0af67
Super N. Ova <supernova@supernova.lco.gtn>
2017-08-11 22:01:32 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nrespipe',
+      author=['Rob Siverd'],
+      author_email=['rsiverd@lco.global'],
+      version=0.1,
+      py_moduldes=['pf_model'],
+      install_requires=['numpy', 'pandas', 'statsmodels'],)
